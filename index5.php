ef51cba5cffbbb157f35e6fce9509fd2d5a48274
Valentina <valentinaolmosoo@gmail.com>
2025-10-01 14:23:03 -0300

    Ej 5 terminado

--- index5.php
+++ index5.php
@@ -4,26 +4,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio 1:</title>
-    <!--  Sumar números pares hasta un número dado por el usuario -->
+    <title>Ejercicio 5:</title>
+    <!-- Busca un número ingresado por el usuario en un arreglo predefinido y retorne la posición en la cual se encuentra este número. -->
 </head>
 
 <body>
     <form method="post">
-        <label for="numero">Ingrese un número:</label>
+        <label for="numero">Ingrese un número (del 1 al 10):</label>
         <input type="number" name="numero" id="numero" required>
-        <button type="submit">Calcular</button>
+        <button type="submit">Buscar en array</button>
     </form>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $numero = intval($_POST["numero"]);
-        $suma = 0;
-
-        for ($i = 2; $i <= $numero; $i += 2) {
-            $suma += $i;
+        $arregloPredefinido = [3, 7, 10, 2, 9, 6, 1, 4, 8, 5];
+        $posicion= array_search($numero, $arregloPredefinido);
+        if ($posicion === false) {
+            echo "<p>El número $numero es invalido, tiene que ser del 1 al 10.</p>";
+            exit;
+        } else {
+            echo "<p>El número $numero se encuentra en la posición $posicion del arreglo.</p>";
         }
 
-        echo "<p>La suma de los números pares hasta $numero es: $suma </p>";
     }
     ?>
 </body>
